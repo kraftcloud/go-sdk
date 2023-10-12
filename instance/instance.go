f085d70cb13d7944f1fb3ae098093c09953ca330
Alexander Jung <alex@kraft.cloud>
2023-10-12 14:31:03 +0200

    style(instance): Add comments to `Instance` structure
    Signed-off-by: Alexander Jung <alex@kraft.cloud>

--- instance/instance.go
+++ instance/instance.go
@@ -105,22 +105,59 @@ type ConsoleOutputResponse struct {
 	} `json:"data"`
 }
 
-// Instance holds the description of the Kraftcloud compute instance, as understood by the API server.
+// Instance holds the description of the KraftCloud compute instance, as
+// understood by the API server.
+//
+// See: https://docs.kraft.cloud/002-rest-api-v1-instances.html#response_2
 type Instance struct {
-	UUID              string             `json:"uuid" pretty:"UUID"`
-	DNS               string             `json:"dns" pretty:"DNS"`
-	PrivateIP         string             `json:"private_ip" pretty:"PrivateIP"`
-	Status            string             `json:"status" pretty:"Status"`
-	CreatedAt         string             `json:"created_at" pretty:"Created At"`
-	Image             string             `json:"image" pretty:"Image"`
-	MemoryMB          int                `json:"memory_mb" pretty:"Memory (MB)"`
-	Args              []string           `json:"args" pretty:"Args"`
-	Env               map[string]string  `json:"env" pretty:"Env"`
-	ServiceGroup      string             `json:"service_group" pretty:"Service Group"`
+	// UUID of the instance.
+	UUID string `json:"uuid" pretty:"UUID"`
+
+	// Publicly accessible DNS name of the instance.
+	DNS string `json:"dns" pretty:"DNS"`
+
+	// Private IPv4 of the instance in CIDR notation for communication between
+	// instances of the same user. This is equivalent to the IPv4 address of the
+	// first network interface.
+	PrivateIP string `json:"private_ip" pretty:"PrivateIP"`
+
+	// Current state of the instance or error if the request failed.
+	Status string `json:"status" pretty:"Status"`
+
+	// Date and time of creation in ISO8601.
+	CreatedAt string `json:"created_at" pretty:"Created At"`
+
+	// Digest of the image that the instance uses.  Note that the image tag (e.g.,
+	// latest) is translated by KraftCloud to the image digest that was assigned
+	// the tag at the time of instance creation. The image is pinned to this
+	// particular version.
+	Image string `json:"image" pretty:"Image"`
+
+	// Amount of memory assigned to the instance in megabytes.
+	MemoryMB int `json:"memory_mb" pretty:"Memory (MB)"`
+
+	// Application arguments.
+	Args []string `json:"args" pretty:"Args"`
+
+	// Key/value pairs to be set as environment variables at boot time.
+	Env map[string]string `json:"env" pretty:"Env"`
+
+	// UUID of the service group that the instance is part of.
+	ServiceGroup string `json:"service_group" pretty:"Service Group"`
+
+	// List of network interfaces attached to the instance.
 	NetworkInterfaces []NetworkInterface `json:"network_interfaces" pretty:"Network Interfaces"`
-	BootTimeUS        int64              `json:"boot_time_us" pretty:"Boot Time (ms)"`
-	Message           string             `json:"message"`
-	Error             int64              `json:"error"`
+
+	// Time it took to start the instance including booting Unikraft in
+	// microseconds.
+	BootTimeUS int64 `json:"boot_time_us" pretty:"Boot Time (ms)"`
+
+	// When an instance has a specific issue an accompanying message is included
+	// to help diagnose the state of the instance.
+	Message string `json:"message"`
+
+	// An error response code dictating the specific error type.
+	Error int64 `json:"error"`
 }
 
 func (i *Instance) GetFieldByPrettyTag(tag string) string {
