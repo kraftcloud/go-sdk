fd1a5b92705e10230cc71b044b4f9607468402ff
Alexander Jung <alex@kraft.cloud>
2023-10-12 16:17:14 +0200

    fix(instance): Add `omitempty` attribute to JSON tags in `Instance`
    Signed-off-by: Alexander Jung <alex@kraft.cloud>

--- instance/instance.go
+++ instance/instance.go
@@ -26,56 +26,56 @@ type NetworkInterface struct {
 // See: https://docs.kraft.cloud/002-rest-api-v1-instances.html#response_2
 type Instance struct {
 	// UUID of the instance.
-	UUID string `json:"uuid" pretty:"UUID"`
+	UUID string `json:"uuid,omitempty" pretty:"UUID"`
 
 	// Publicly accessible DNS name of the instance.
-	DNS string `json:"dns" pretty:"DNS"`
+	DNS string `json:"dns,omitempty" pretty:"DNS"`
 
 	// Private IPv4 of the instance in CIDR notation for communication between
 	// instances of the same user. This is equivalent to the IPv4 address of the
 	// first network interface.
-	PrivateIP string `json:"private_ip" pretty:"PrivateIP"`
+	PrivateIP string `json:"private_ip,omitempty" pretty:"PrivateIP"`
 
 	// Current state of the instance or error if the request failed.
-	Status string `json:"status" pretty:"Status"`
+	Status string `json:"status,omitempty" pretty:"Status"`
 
 	// Date and time of creation in ISO8601.
-	CreatedAt string `json:"created_at" pretty:"Created At"`
+	CreatedAt string `json:"created_at,omitempty" pretty:"Created At"`
 
 	// Digest of the image that the instance uses.  Note that the image tag (e.g.,
 	// latest) is translated by KraftCloud to the image digest that was assigned
 	// the tag at the time of instance creation. The image is pinned to this
 	// particular version.
-	Image string `json:"image" pretty:"Image"`
+	Image string `json:"image,omitempty" pretty:"Image"`
 
 	// Amount of memory assigned to the instance in megabytes.
-	MemoryMB int `json:"memory_mb" pretty:"Memory (MB)"`
+	MemoryMB int `json:"memory_mb,omitempty" pretty:"Memory (MB)"`
 
 	// Application arguments.
-	Args []string `json:"args" pretty:"Args"`
+	Args []string `json:"args,omitempty" pretty:"Args"`
 
 	// Key/value pairs to be set as environment variables at boot time.
-	Env map[string]string `json:"env" pretty:"Env"`
+	Env map[string]string `json:"env,omitempty" pretty:"Env"`
 
 	// UUID of the service group that the instance is part of.
-	ServiceGroup string `json:"service_group" pretty:"Service Group"`
+	ServiceGroup string `json:"service_group,omitempty" pretty:"Service Group"`
 
 	// List of network interfaces attached to the instance.
-	NetworkInterfaces []NetworkInterface `json:"network_interfaces" pretty:"Network Interfaces"`
+	NetworkInterfaces []NetworkInterface `json:"network_interfaces,omitempty" pretty:"Network Interfaces"`
 
 	// Time it took to start the instance including booting Unikraft in
 	// microseconds.
-	BootTimeUS int64 `json:"boot_time_us" pretty:"Boot Time (ms)"`
+	BootTimeUS int64 `json:"boot_time_us,omitempty" pretty:"Boot Time (ms)"`
 
 	// When an instance has a specific issue an accompanying message is included
 	// to help diagnose the state of the instance.
-	Message string `json:"message"`
+	Message string `json:"message,omitempty"`
 
 	// An error response code dictating the specific error type.
-	Error int64 `json:"error"`
+	Error int64 `json:"error,omitempty"`
 
 	// Base 64 encoded console output.
-	Output string `json:"output,omitempty"`
+	Output string `json:"output,omitempty,omitempty"`
 }
 
 func (i *Instance) GetFieldByPrettyTag(tag string) string {
