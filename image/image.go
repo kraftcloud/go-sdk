b1f68ce5d315aa6054dd31988d2056a597dbb568
Alexander Jung <alex@kraft.cloud>
2023-10-13 09:39:30 +0200

    docs(image): Add comments to `Image` as described by the API docs
    [0]: https://docs.kraft.cloud/004-rest-api-v1-images.html
    
    Signed-off-by: Alexander Jung <alex@kraft.cloud>

--- image/image.go
+++ image/image.go
@@ -13,15 +13,32 @@ const (
 	Endpoint = "/images"
 )
 
-// Image describes a Kraftcloud image as returned by the API server.
+// Image describes a KraftCloud image as returned by the API server.
 type Image struct {
-	Digest      string   `json:"digest" pretty:"Digest"`
-	Tags        []string `json:"tags" pretty:"Tags"`
-	Public      bool     `json:"public" pretty:"Public"`
-	Initrd      bool     `json:"initrd" pretty:"Initrd"`
-	SizeInBytes int64    `json:"size_in_bytes" pretty:"Size (bytes)"`
-	Args        string   `json:"args" pretty:"Args"`
-	KernelArgs  string   `json:"kernel_args" pretty:"Kernel Args"`
+	// Image digest to uniquely identify this image.
+	Digest string `json:"digest" pretty:"Digest"`
+
+	// Tags referencing this image. Can be used to create instances from this
+	// image.
+	Tags []string `json:"tags" pretty:"Tags"`
+
+	// Indicates if this is a public image. If true every KraftCloud user can
+	// access it
+	Public bool `json:"public" pretty:"Public"`
+
+	// Indicates if the image comes with an init ramdisk.
+	Initrd bool `json:"initrd" pretty:"Initrd"`
+
+	// Total size of the image on disk in bytes including the initrd, if any.
+	SizeInBytes int64 `json:"size_in_bytes" pretty:"Size (bytes)"`
+
+	// Application arguments hardcoded into the image. Prepended to the arguments
+	// of any instance running the image.
+	Args string `json:"args" pretty:"Args"`
+
+	// Unikraft kernel arguments hardcoded into the image. Prepended to the kernel
+	// arguments set by KraftCloud
+	KernelArgs string `json:"kernel_args" pretty:"Kernel Args"`
 }
 
 func (i *Image) GetFieldByPrettyTag(tag string) string {
