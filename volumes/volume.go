19ef66fa5a2c4cbf666030938d2eda030a52da0c
Alexander Jung <alex@kraft.cloud>
2023-12-01 17:15:52 +0100

    fix(volumes)!: Remove "success" and "failed" states
    These in fact represent the overall state of the API call and not
    the state of the volume.
    
    Signed-off-by: Alexander Jung <alex@kraft.cloud>

--- volumes/volume.go
+++ volumes/volume.go
@@ -32,13 +32,6 @@ const (
 
 	// There are maintenance tasks running on the volume.
 	StateBusy = State("busy")
-
-	// The volume is in an error state that needs inspection by a KraftCloud
-	// engineer.
-	StateError = State("error")
-
-	// The request was successful.
-	StateSuccess = State("success")
 )
 
 // VolumeAttachedToInstance represents an instance that a volume is attached to.
@@ -58,9 +51,6 @@ type VolumeAttachedToInstance struct {
 // can be initialized with a set of files with one instance and then be detached
 // and attached to a another one.
 type Volume struct {
-	// Current state of the volume (see states) or error if the request failed
-	Status State `json:"status,omitempty"`
-
 	// UUID of the volume.
 	UUID string `json:"uuid,omitempty"`
 
@@ -69,6 +59,12 @@ type Volume struct {
 
 	// List of instances that this volume is attached to.
 	AttachedTo []VolumeAttachedToInstance `json:"attached_to,omitempty"`
+
+	// Current state of the volume (see states) or error if the request failed
+	State State `json:"State,omitempty"`
+
+	// Status indicates the success or failure of the response.
+	Status string `json:"status,omitempty"`
 
 	// Message contains the error message either on `partial_success` or `error`.
 	Message string `json:"message,omitempty"`
