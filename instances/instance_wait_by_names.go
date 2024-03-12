99ec6331d51005970b8f0254b4c53b2cc3312422
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2024-03-12 14:24:58 +0200

    feat(instances): Introduce instance wait endpoint
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

---
+++ instances/instance_wait_by_names.go
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2023, Unikraft GmbH.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+package instances
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	kcclient "sdk.kraft.cloud/client"
+)
+
+// WaitByNames implements InstancesService.
+func (c *client) WaitByNames(ctx context.Context, state State, timeoutMs int, names ...string) ([]WaitResponseItem, error) {
+	if len(names) == 0 {
+		return nil, errors.New("requires at least one name")
+	}
+
+	reqItems := make([]map[string]any, 0, len(names))
+	for _, name := range names {
+		reqItems = append(reqItems, map[string]any{
+			"name":       name,
+			"state":      state,
+			"timeout_ms": timeoutMs,
+		})
+	}
+
+	body, err := json.Marshal(reqItems)
+	if err != nil {
+		return nil, fmt.Errorf("encoding JSON object: %w", err)
+	}
+
+	var resp kcclient.ServiceResponse[WaitResponseItem]
+	if err := c.request.DoRequest(ctx, http.MethodGet, Endpoint+"/wait", bytes.NewReader(body), &resp); err != nil {
+		return nil, fmt.Errorf("performing the request: %w", err)
+	}
+
+	items, err := resp.AllOrErr()
+	if err != nil {
+		errs := make([]error, 0, len(items)+1)
+		errs = append(errs, err)
+		for _, item := range items {
+			if item.Error != nil {
+				errs = append(errs, fmt.Errorf("%s (code=%d)", item.Message, *item.Error))
+			}
+		}
+		return nil, errors.Join(errs...)
+	}
+	return items, nil
+}
