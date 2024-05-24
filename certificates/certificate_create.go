8688833d6c2818620a839bbfc2ac0cfb529a7301
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2024-05-24 14:11:34 +0300

    feat(certificates): Add new POST request endpoint
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

---
+++ certificates/certificate_create.go
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2024, Unikraft GmbH.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+package certificates
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	kcclient "sdk.kraft.cloud/client"
+)
+
+// Create implements InstancesService.
+func (c *client) Create(ctx context.Context, req *CreateRequest) (*kcclient.ServiceResponse[CreateResponseItem], error) {
+	body, err := json.Marshal(req)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling request body: %w", err)
+	}
+
+	resp := &kcclient.ServiceResponse[CreateResponseItem]{}
+	if err := c.request.DoRequest(ctx, http.MethodPost, Endpoint, bytes.NewReader(body), resp); err != nil {
+		return nil, fmt.Errorf("performing the request: %w", err)
+	}
+
+	return resp, nil
+}
