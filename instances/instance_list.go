318a516b2a41a07f26c76cb780beabd52d71cf79
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2025-07-07 12:58:56 +0300

    feat(instances,volumes)!: Add option to use tags
    This also slightly changes the interface call for Get/List making this
    a breaking change for people using the sdk.
    
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

--- instances/instance_list.go
+++ instances/instance_list.go
@@ -9,14 +9,21 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 
 	kcclient "sdk.kraft.cloud/client"
 )
 
 // List implements InstancesService.
-func (c *client) List(ctx context.Context) (*kcclient.ServiceResponse[GetResponseItem], error) {
+func (c *client) List(ctx context.Context, tags []string) (*kcclient.ServiceResponse[GetResponseItem], error) {
 	resp := &kcclient.ServiceResponse[GetResponseItem]{}
-	if err := c.request.DoRequest(ctx, http.MethodGet, Endpoint, nil, resp); err != nil {
+
+	endpoint := Endpoint
+	if len(tags) > 0 {
+		endpoint = fmt.Sprintf("%s?tags=%s", Endpoint, strings.Join(tags, ","))
+	}
+
+	if err := c.request.DoRequest(ctx, http.MethodGet, endpoint, nil, resp); err != nil {
 		return nil, fmt.Errorf("performing the request: %w", err)
 	}
 
